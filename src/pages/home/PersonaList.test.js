428ae70409b532ca19553ee7ef27ee1d241ba68c
p.bonne@philips.com <phnlnly99330@MACC02R24B9G8WP.ddns.htc.nl.philips.com>
2018-03-09 15:14:54 +0100

    Added snapshot test PersonaList

---
+++ src/pages/home/PersonaList.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { PersonaList } from './PersonaList'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <PersonaList>
+        <p>Text</p>
+        <p>Text</p>
+      </PersonaList>
+    ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
