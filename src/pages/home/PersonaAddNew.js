1850ef21d9e1e5d1c509e770ceb87d7b81ecace0
Patrick <p.bonne@hilips.com>
2018-03-12 18:50:50 +0100

    Add missed file

---
+++ src/pages/home/PersonaAddNew.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class PersonaAddNew extends React.Component {
+    
+    state = {
+        enteredName:''
+    }
+
+    onAddName () {
+        this.props.personaNew(this.state.enteredName)
+        this.setState( {enteredName:''} )
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Add new persona</h1>
+                Name: <input value={this.state.enteredName} onChange={(evt) => this.setState( {enteredName:evt.target.value} )} type='text' />
+                <br/>
+                <button onClick={this.onAddName.bind(this)} disabled={this.state.enteredName===''}>Add</button>
+            </div>
+        )
+    }
+}
+
+export { PersonaAddNew }
